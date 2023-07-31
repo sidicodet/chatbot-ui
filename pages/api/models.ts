07902ef5784dbcc11b69f3edcaab66609d5a9674
sidicodet <121307234+sidicodet@users.noreply.github.com>
2023-07-31 20:56:28 +0100

    models.ts

--- pages/api/models.ts
+++ pages/api/models.ts
@@ -69,4 +69,18 @@ const handler = async (req: Request): Promise<Response> => {
   }
 };
 
+app.post("/create", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "512x512",
+    });
+    res.send(response.data.data[0].url);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
 export default handler;
